a482e13d9a6ecd2df37dd520c9ff9e7be2dc5f10
HAGENSV22 <hagensv22@gcc.edu>
2025-02-23 22:28:36 -0500

    new play method which returns results

--- src/main/java/edu/games/tic_tac_toe/TicTacToeGame.java
+++ src/main/java/edu/games/tic_tac_toe/TicTacToeGame.java
@@ -1,35 +1,73 @@
 package edu.games.tic_tac_toe;
 
 public class TicTacToeGame {
-    TicTacToeAI player1;
-    TicTacToeAI player2;
-    TicTacToeBoard board;
+    private final Class<? extends TicTacToeAI> ai1;
+    private final Class<? extends TicTacToeAI> ai2;
 
     public static void main(String[] args) {
         new TicTacToeGame(RandomAI.class,RandomAI.class);
     }
 
     public TicTacToeGame(Class<? extends TicTacToeAI> ai1, Class<? extends TicTacToeAI> ai2){
+        this.ai1 = ai1;
+        this.ai2 = ai2;
+    }
 
-        board = new TicTacToeBoard();
+    public Class<? extends TicTacToeAI> play(){
+        TicTacToeBoard board = new TicTacToeBoard();
+        TicTacToeAI player1;
+        TicTacToeAI player2;
         try {
             player1 = (TicTacToeAI) ai1.getConstructors()[0].newInstance('X');
             player2 = (TicTacToeAI) ai2.getConstructors()[0].newInstance('O');
         } catch (Exception e){
             System.out.println("Failed to initialize AI");
+            return null;
         }
 
         boolean p1turn = true;
-        while (!board.isFull()){
+        char winner = ' ';
+        boolean forfeit = false;
+        while (!board.isFull() && winner == ' ' && !forfeit){
             TicTacToeBoard copy = new TicTacToeBoard(board);
 
             if (p1turn) {
-                player1.play(copy);
+                for (int i = 1; i <= 10; i++) {
+                    int move = player1.play(copy);
+                    if (board.setVal(move,'X')){
+                        break;
+                    }
+                    if (i == 10){
+                        forfeit = true;
+                        winner = 'O';
+                    }
+                }
             } else {
-                player2.play(copy);
+                for (int i = 1; i <= 10; i++) {
+                    int move = player2.play(copy);
+                    if (board.setVal(move,'O')){
+                        break;
+                    }
+                    if (i == 10){
+                        forfeit = true;
+                        winner = 'X';
+                    }
+                }
             }
 
-            p1turn = !p1turn;
+            if (!forfeit) {
+                winner = board.checkWin();
+                p1turn = !p1turn;
+            }
+        }
+
+        if (winner == 'X'){
+            return ai1;
+        }
+        else if (winner == 'O'){
+            return ai2;
+        } else {
+            return null;
         }
     }
 }
