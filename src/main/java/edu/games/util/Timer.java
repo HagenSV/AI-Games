33ae7cb6267e6982bd10d93b98c8eb9ba21eaead
HAGENSV22 <hagensv22@gcc.edu>
2025-02-24 00:01:14 -0500

    Add timer class

---
+++ src/main/java/edu/games/util/Timer.java
@@ -0,0 +1,37 @@
+package edu.games.util;
+
+/**
+ * Class dedicated to measuring time differences
+ */
+public class Timer {
+
+    private static Timer instance;
+    private long startTime;
+
+    private Timer(){
+        startTime = 0;
+    }
+
+    public Timer getInstance(){
+        if (instance == null){
+            instance = new Timer();
+        }
+        return instance;
+    }
+
+    /**
+     * Starts the timer at the current time;
+     */
+    public void start(){
+        startTime = System.nanoTime();
+    }
+
+    /**
+     * Stops the timer and returns the current time
+     * @return the current time in nanoseconds
+     */
+    public long stop(){
+        long endTime = System.nanoTime();
+        return endTime -startTime;
+    }
+}
